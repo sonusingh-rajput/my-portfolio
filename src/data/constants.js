e9fa3c2c38cdb0f2cca339f541af76130513595d
Sonu Singh <sonusinghrajput270@gmail.com>
2025-01-02 14:46:57 +0530

    portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,197 @@
+export const Bio = {
+  name: "Sonu Singh",
+  roles: ["Web Developer", "Programmer"],
+  description: `I am a web developer who loves creating user-friendly and
+ responsive websites. I enjoy turning ideas into functional
+ web applications using the latest technologies. My focus is
+ on delivering high-quality work that improves user
+ experience and helps businesses grow. `,
+  github: "https://github.com/sonusingh-rajput",
+  gmail: "sonusinghrajput270@gmail.com",
+  resume:
+    "https://drive.google.com/file/d/1UmQBK-mk0nYqjR8iTJ27GQUFIYNVsd3d/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/sonu-singh-rajput",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          " https://tailwindcss.com/_next/static/media/tailwindcss-mark.3c5441fc7a190fb1800d4a5c7f07ba4b1345a9c8.svg",
+      },
+      {
+        name: "Shadcn UI",
+        image: "https://ui.shadcn.com/apple-touch-icon.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image: "https://img.icons8.com/color/512/express-js.png",
+      },
+
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Java_programming_language_logo.svg/242px-Java_programming_language_logo.svg.png",
+      },
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=1200,h=630,fit=crop,f=jpeg/AQE8brWqjXHjajq8/wizzoi-mP4PqQlP1DuB0X7L.png",
+    role: "Web Developer",
+    company: "Wizzoi",
+    date: "",
+    desc: `Completely redesigned and developed Wizzoi corporate
+            website, enhancing UI/UX and aligning with modern design
+            trends. 
+            I applied responsive design principles to guarantee a
+            seamless user experience across all devices. 
+            Improved website ranking on search engines, boosting
+            online visibility and lead generation.`,
+    skills: [
+      "vue js",
+      "Redux",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "API Integration",
+      "Bootstrap",
+    ],
+  },
+  {
+    id: 5,
+    img: "https://maalakshmicare.com/wp-content/uploads/2024/09/Maa-Lakshmi-Care-Logo-300-x-150-px.png",
+    role: "Web Developer ",
+    company: "Flexiona",
+    date: " ",
+    desc: `Designed and developed a user-friendly website. 
+Implemented responsive design to ensure seamless access
+ across mobile, tablet, and desktop devices. 
+Optimized the website for SEO, improving local search
+ visibility and patient reach. 
+Created a modern and professional aesthetic that reflects
+ the clinic’s reputation and services. `,
+    skills: [
+      "Wordpress",
+      "Elementor",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "SEO",
+      "Website Administrator",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: " ",
+    school: "Tilka Manjhi Bhagalpur University(Bhagalpur)",
+    date: "2014 - 2017",
+    Per: "  ",
+    desc: " ",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Estate Website",
+    description:
+      "Estate is a modern real estate website built with React and styled using Tailwind CSS. It offers a responsive design, dynamic property showcases, and intuitive navigation, providing a seamless platform for buyers, sellers, and agents.",
+    image: "../images/Project Image/Estate.png",
+    tags: ["React Js", "Tailwind CSS", "Javascript", "CSS"],
+    category: "web app",
+    github: "https://github.com/sonusingh-rajput/Estate-Website",
+    webapp: "https://estate-website-psi.vercel.app/",
+  },
+  {
+    id: 12,
+    title: "My Portfolio",
+    description:
+      "My Portfolio is a React.js application that serves as an online portfolio to showcase my work, skills, and achievements. It provides an overview of my background, displays my projects, and offers a way to contact me.",
+    image: "",
+    tags: ["React Js", "HTML5", "Tailwind CSS", "Javascript"],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+];
